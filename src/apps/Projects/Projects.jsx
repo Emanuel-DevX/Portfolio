f608be1474635dffdf9858a1dba73d6dcec7c56e
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-07 20:28:22 -0700

    Design basic filter bar for projects

--- src/apps/Projects/Projects.jsx
+++ src/apps/Projects/Projects.jsx
@@ -1,5 +1,23 @@
-const Projects = function(){
-    return (<></>)
-
+import { useState } from 'react'
+import ProjectFilterBar from './ProjectFilterBar'
+const Projects = function () {
+  const [filters, setFilters] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+  
+   
+     return (<>
+ 
+   
+           <ProjectFilterBar
+             searchInput={searchInput}
+             setSearchInput={setSearchInput}
+             filters={filters}
+             setFilters={setFilters}
+           />
+   
+  
+      <div className='h-800'></div>
+    </>
+  )
 }
-export default Projects;
+export default Projects
