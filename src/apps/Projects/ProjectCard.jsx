0db56e0b917c7c88d4e38745430204ead5b18b03
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-08 00:31:31 -0700

    Add group project indicaters with role specification

--- src/apps/Projects/ProjectCard.jsx
+++ src/apps/Projects/ProjectCard.jsx
@@ -1,10 +1,29 @@
 import { Link } from 'lucide-react'
 import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa'
+import { PiPersonSimple, PiUsersThree } from 'react-icons/pi'
 
 const ProjectCard = function ({ project }) {
   return (
     <>
-      <div className="md:w-[32%] bg-black/50 rounded-2xl flex flex-col">
+      <div className="md:w-[32%] bg-black/50 rounded-2xl flex flex-col relative ">
+        <div className="absolute text-lg top-2 left-2 p-1 rounded-md bg-zinc-900 font-bold">
+          {project.isGroup ? (
+            <span title="Group Project" className="flex items-center gap-1">
+              <PiUsersThree /> <span className="text-xs">Group</span>
+            </span>
+          ) : (
+            <span title="Personal Project" className="flex items-center gap-1">
+              <PiPersonSimple /> <span className="text-xs">Personal</span>
+            </span>
+          )}
+        </div>
+        <div className="absolute top-2 right-2 font-bold bg-zinc-900 p-1 px-2 rounded-full text-zinc-200">
+          {project.isGroup && project.role && (
+            <span title={`Role: ${project.role}`} className="block text-xs ">
+              {project.role}
+            </span>
+          )}
+        </div>
         <img
           className="rounded-t-2xl md:max-h-44 lg:max-h-48 w-full object-cover"
           src={project.image}
@@ -21,7 +40,7 @@ const ProjectCard = function ({ project }) {
                 </div>
               ))}
           </div>
-          <div className="mt-3 flex text-2xl gap-3">
+          <div className="mt-3 flex  gap-3">
             {project.github && (
               <a
                 className="text-2xl"
@@ -37,13 +56,14 @@ const ProjectCard = function ({ project }) {
 
             {project.url && (
               <a
+                className="flex text-sm"
                 href={project.url}
                 target="_blank"
                 rel="noopener noreferrer"
                 title="View Live Demo"
                 aria-label="Live Demo"
               >
-                <FaExternalLinkAlt />
+                <Link />
               </a>
             )}
           </div>
