c9651d132ec95492d91a0e861da6d54ef788dd74
Emanuel-DevX <jefymoxi@gmail.com>
2025-08-22 21:39:31 -0700

    Update ChatBubble: improve text wrapping and overflow handling for chat messages

--- src/apps/Contact/ChatBubble.jsx
+++ src/apps/Contact/ChatBubble.jsx
@@ -1,9 +1,8 @@
-// ChatBubble.jsx
 import React from 'react'
 
 const ChatBubble = ({ from, text }) => (
   <div
-    className={`max-w-xs md:max-w-md px-4 py-2 rounded-xl mb-3 ${
+    className={`max-w-xs md:max-w-md px-4 py-2 rounded-xl mb-3 break-words whitespace-pre-wrap  ${
       from === 'kiya'
         ? 'bg-zinc-800 text-white self-start rounded-bl-none'
         : 'bg-gradient-to-br from-yellow-400 via-orange-400 to-yellow-400 text-black self-end rounded-br-none'
