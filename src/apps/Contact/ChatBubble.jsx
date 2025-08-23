52be722d22bf54c69fc775bd9cd4d4d5cbdb5ccb
Emanuel-DevX <jefymoxi@gmail.com>
2025-08-22 22:48:29 -0700

    Update ChatBubble: add better text wrapping and scrolling for long bubbles

--- src/apps/Contact/ChatBubble.jsx
+++ src/apps/Contact/ChatBubble.jsx
@@ -5,7 +5,7 @@ const ChatBubble = ({ from, text }) => (
     className={`max-w-xs md:max-w-md px-4 py-2 rounded-xl mb-3 break-words whitespace-pre-wrap  ${
       from === 'kiya'
         ? 'bg-zinc-800 text-white self-start rounded-bl-none'
-        : 'bg-gradient-to-br from-yellow-400 via-orange-400 to-yellow-400 text-black self-end rounded-br-none'
+        : 'bg-gradient-to-br from-yellow-500  to-yellow-400 text-black self-end rounded-br-none'
     }`}
   >
     {text}
